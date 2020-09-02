e5c18bc7cc3f2842dff7bd045877c4ab30413153
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2020-09-02 20:17:57 -0300

    feature/signUp page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <>
-      <Signin />
+      <Signup />
       <GlobalStyle />
     </>
   );

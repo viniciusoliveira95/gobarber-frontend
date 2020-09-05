a99f6e7822e0e82711f1b4274e1ba10b234e843a
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2020-09-05 19:31:42 -0300

    feature/login-logout

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import Signin from './pages/Signin';
 // import Signup from './pages/Signup';
+import { AuthProvider } from './hooks/AuthContext';
 
-function App() {
-  return (
-    <>
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
       <Signin />
-      <GlobalStyle />
-    </>
-  );
-}
+    </AuthProvider>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
